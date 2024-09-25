be59ab627e6b2a4d9d75e44ea68a4a9345c671b8
Charith Wickramasinghe <pvmcharith@gmail.com>
2024-09-25 19:54:09 +0530

    update database.sql

--- database.sql
+++ database.sql
@@ -2,15 +2,16 @@ CREATE DATABASE `account_system`;
 USE `account_system`;
 
 CREATE TABLE `users` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `fname` varchar(50) NOT NULL,
   `lname` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
   `otp_code` varchar(6) DEFAULT NULL,
   `is_verified` tinyint(1) DEFAULT '0',
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
-)
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+);
 
 
 -- package install command:  composer install
